3e8b61970637fe42f968e54344f5ebda95743de1
Dena's MacBook <djclink@ucdavis.edu>
2020-08-02 15:16:04 -0400

    Update camera trap function

---
+++ R/CameraTrapAnnotations.R
@@ -0,0 +1,18 @@
+#' Function to create a table of available camera trap annotations by season
+#'
+#' @param season which season to download; options are 1-4, 6-8,10,11
+#'
+#' @return
+#' @export
+#'
+#' @examples
+CameraTrapAnnotations <- function( season= 4){
+
+dataSet <- paste('season',season,'df','.rda',sep = '')
+
+githubURL <- paste('https://github.com/DenaJGibbon/behaviouRdata/raw/master/data/',dataSet,sep='')
+CameraTrapDf <- get(load(url(githubURL)))
+
+return(table(CameraTrapDf$category_id))
+}
+
